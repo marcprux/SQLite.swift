a580c5a7515a5b5e1bb4fae076c1862c0960d14a
Marc Prud'hommeaux <marc@skip.tools>
2025-01-07 12:29:45 -0500

    Use conditional os Android guards

--- Tests/SQLiteTests/Typed/CustomFunctionsTests.swift
+++ Tests/SQLiteTests/Typed/CustomFunctionsTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import SQLite
 
 // https://github.com/stephencelis/SQLite.swift/issues/1071
-#if !os(Linux) && !canImport(Android)
+#if !os(Linux) && !os(Android)
 
 class CustomFunctionNoArgsTests: SQLiteTestCase {
     typealias FunctionNoOptional              = () -> SQLite.Expression<String>
